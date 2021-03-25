a36d988ef8bfb3034150d9a2b87982a423fe4af2
Hatice <haticeevci@gmail.com>
2021-03-25 00:49:30 -0400

    manifest into html

--- public/index.js
+++ public/index.js
@@ -136,6 +136,7 @@ function sendTransaction(isAdding) {
   })
   .catch(err => {
     // fetch failed, so save in indexed db
+    console.log("fetch failed, so save in indexed db");
     saveRecord(transaction);
 
     // clear form
